ef429f2fa1301429949bbbab540ac5671e66f1c3
17316267816 <origerous@live.com>
2025-05-29 21:05:29 +0800

    123

--- ai/trend/models/tabnet_model_trainer.py
+++ ai/trend/models/tabnet_model_trainer.py
@@ -213,7 +213,7 @@ def train_whole_market():
         patience=10,
         num_workers=4,
         batch_size=256,
-        virtual_batch_size=128,
+        virtual_batch_size=256,
         max_epochs=20
     )
     y_pred = model.predict_proba(X_valid.to_numpy())[:, -1]
