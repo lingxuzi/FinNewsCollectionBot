24397c1323d78709cd82353b46b1a0c2acbf0f2d
17316267816 <origerous@live.com>
2025-06-15 14:41:17 +0800

    123

--- ai/embedding/dataset.py
+++ ai/embedding/dataset.py
@@ -9,6 +9,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from torch.utils.data import Dataset
 from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler
 from utils.common import read_text
+from utils.lmdb import LMDBEngine
 from tqdm import tqdm
 from diskcache import FanoutCache
 
@@ -61,7 +62,8 @@ class KlineDataset(Dataset):
         self.noise_level = 1e-3
         self.tag = tag
         os.makedirs(os.path.join(db_path, f'{tag}'), exist_ok=True)
-        self.cache = FanoutCache(os.path.join(db_path, f'{tag}'), shards=16, timeout=5, size_limit=3e11, eviction_policy='none')
+        # self.cache = FanoutCache(os.path.join(db_path, f'{tag}'), shards=16, timeout=5, size_limit=3e11, eviction_policy='none')
+        self.cache = LMDBEngine(os.path.join(db_path, f'{tag}'))
 
         if self.cache.get('total_count') is None:
             # 1. 从数据库加载数据
@@ -99,13 +101,17 @@ class KlineDataset(Dataset):
                         print(f"Error processing stock {code}: {e}")
             # 3. 清理内存
             for i, (ts_seq, ctx_seq, label) in tqdm(enumerate(zip(self.ts_sequences, self.ctx_sequences, self.labels)), desc="Caching sequences"):
-                self.cache.set(f'seq_{i}', (ts_seq, ctx_seq, label))
-            self.cache.set('total_count', len(self.ts_sequences))
+                self.cache.put(f'seq_{i}', (ts_seq, ctx_seq, label))
+            self.cache.put('total_count', len(self.ts_sequences))
             print(f"Total sequences cached: {self.cache.get('total_count')}")
             del all_data_df  # 释放内存
             del self.ts_sequences
             del self.ctx_sequences
             del self.labels
+        
+            self.cache.commit()  # 确保所有数据都已写入LMDB
+        self.cache.close()
+        self.cache = LMDBEngine(os.path.join(db_path, f'{tag}'), readonly=True)
 
     def generate_sequences(self, code, all_data_df, encoded_categorical):
         ts_sequences = [] # 时间序列部分
@@ -143,11 +149,11 @@ class KlineDataset(Dataset):
         return results
                 
     def __len__(self):
-        return self.cache.get('total_count', 0)
+        return self.cache.get('total_count')
     
     def __getitem__(self, idx):
         # t = time.time()
-        ts_seq, ctx_seq, label = self.cache.get(f'seq_{idx}', (None, None, None))
+        ts_seq, ctx_seq, label = self.cache.get(f'seq_{idx}')
         # print(f"Cache access time: {time.time() - t:.4f} seconds")
         if ts_seq is None or ctx_seq is None or label is None:
             raise IndexError("Index out of range or data not found in cache.")
---
+++ utils/lmdb.py
@@ -0,0 +1,39 @@
+import warnings
+import psutil
+import lmdb
+import numpy as np
+import pickle
+
+MAX_SIZE = int(1e12)
+
+class LMDBEngine:
+    def __init__(self, lmdb_path, readonly=False):
+        if not readonly:
+            self.env = lmdb.open(lmdb_path, map_size=MAX_SIZE, map_async=True, writemap=True, meminit=False)
+        else:
+            self.env = lmdb.open(lmdb_path, readonly=True, lock=False, readahead=False, meminit=False)
+        
+        if readonly:
+            self.txn = self.env.begin()
+        else:
+            self.txn = self.env.begin(write=True)
+            
+    @property
+    def is_readonly(self):
+        return self.env.readonly
+    
+    def commit(self):
+        self.txn.commit()
+        self.txn = None
+        self.txn = self.env.begin(write=True)
+    
+    def put(self, key, val):
+        self.txn.put(key.encode(), pickle.dumps(val))
+
+    def get(self, key):
+        return pickle.loads(self.txn.get(key.encode())) if self.txn.get(key.encode()) else None
+
+    def close(self):
+        if self.txn:
+            self.txn.abort()
+        self.env.close()
