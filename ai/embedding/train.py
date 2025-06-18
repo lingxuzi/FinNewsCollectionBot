30f10e8a5e95dcb022ec28d934ee1f506d820945
17316267816 <origerous@live.com>
2025-06-18 12:58:08 +0800

    123

--- ai/embedding/train.py
+++ ai/embedding/train.py
@@ -117,8 +117,8 @@ def run_training(config):
     criterion_ts = nn.HuberLoss(delta=0.1) # 均方误差损失
     criterion_ctx = nn.HuberLoss(delta=0.1) # 均方误差损失
     criterion_predict = nn.HuberLoss(delta=0.1) # 均方误差损失
-    alpha = 0.2
-    beta = 0.2
+    alpha = config['training']['alpha']  # ctx loss weight
+    beta = config['training']['beta']  # pred loss weight
     optimizer = torch.optim.AdamW(model.parameters(), lr=config['training']['min_learning_rate'], weight_decay=1e-5)
     early_stopper = EarlyStopping(patience=10, direction='up')
     
