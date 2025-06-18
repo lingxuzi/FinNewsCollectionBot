cd216ccc8ed47253421d16bf73b87d8cc2bfe971
17316267816 <origerous@live.com>
2025-06-18 12:05:55 +0800

    123

--- ai/embedding/generate/generate_embedding.py
+++ ai/embedding/generate/generate_embedding.py
@@ -1,4 +1,5 @@
 from pymilvus import MilvusClient, DataType
+from pymilvus.milvus_client.index import IndexParams
 from ai.embedding.model import MultiModalAutoencoder
 from ai.embedding.dataset import KlineDataset
 from torch.utils.data import DataLoader
@@ -13,34 +14,33 @@ def init_indexer(index_db, embedding_dim):
     path = os.path.split(index_db)[0]
     os.makedirs(path, exist_ok=True)
     vec_client = MilvusClient(index_db)
-    if not vec_client.has_collection('kline_embeddings'):
-        shema = vec_client.create_schema(
-            auto_id=True,
-            enable_dynmaic_field=True
-        )
-        shema.add_field(field_name='id', datatype=DataType.INT64, is_primary=True)
-        shema.add_field(field_name='code', datatype=DataType.VARCHAR, max_length=16)
-        shema.add_field(field_name='start_date', datatype=DataType.FLOAT)
-        shema.add_field(field_name='end_date', datatype=DataType.FLOAT)
-        shema.add_field(field_name='embedding', datatype=DataType.FLOAT_VECTOR, dim=embedding_dim)
-        shema.add_field(field_name='industry', datatype=DataType.VARCHAR, max_length=64)
+    if vec_client.has_collection('kline_embeddings'):
+        vec_client.drop_collection('kline_embeddings')
+    shema = vec_client.create_schema(
+        auto_id=True,
+        enable_dynmaic_field=False
+    )
+    shema.add_field(field_name='id', datatype=DataType.INT64, is_primary=True)
+    shema.add_field(field_name='code', datatype=DataType.VARCHAR, max_length=16)
+    shema.add_field(field_name='start_date', datatype=DataType.FLOAT)
+    shema.add_field(field_name='end_date', datatype=DataType.FLOAT)
+    shema.add_field(field_name='embedding', datatype=DataType.FLOAT_VECTOR, dim=embedding_dim)
+    shema.add_field(field_name='industry', datatype=DataType.VARCHAR, max_length=64)
 
-        index = vec_client.create_index(
-            collection_name='kline_embeddings',
-            field_name='embedding',
-            index_params={
-                'index_type': 'IVF_FLAT',
-                'metric_type': 'L2',
-                'params': {'nlist': 1024}
-            }
-        )
+    index_params = vec_client.prepare_index_params()
+    index_params.add_index('id', index_type='STL_SORT')
+    # index_params.add_index('code', index_type='STL_SORT')
+    # index_params.add_index('start_date', index_type=)
+    # index_params.add_index('end_date', index_type='STL_SORT')
+    # index_params.add_index('industry', index_type='STL_SORT')
+    index_params.add_index('embedding', index_type='IVF_FLAT', metric_type='L2', params={'nlist': 1024})
 
-        vec_client.create_collection(
-            auto_id=True,
-            collection_name='kline_embeddings',
-            schema=shema,
-            index_params=index
-        )
+    vec_client.create_collection(
+        auto_id=True,
+        collection_name='kline_embeddings',
+        schema=shema,
+        index_params=index_params
+    )
     return vec_client
 
 def run(config):
@@ -74,6 +74,7 @@ def run(config):
             features=config['embedding']['data']['features'],
             numerical=config['embedding']['data']['numerical'],
             categorical=config['embedding']['data']['categorical'],
+            include_meta=config['embedding']['data']['include_meta'],
             scaler=scaler,
             encoder=encoder,
             is_train=False,
@@ -92,7 +93,7 @@ def run(config):
             industry = encoder.inverse_transform(ctx_sequences[:, -1].cpu().numpy().astype('int32'))  # 假设最后一列是行业信息
 
             with torch.no_grad():
-                _, _, predict_output, final_embedding = model(ts_sequences, ctx_sequences)
+                _, _, predict_output, final_embedding, _ = model(ts_sequences, ctx_sequences)
 
                 data = [
                     {
