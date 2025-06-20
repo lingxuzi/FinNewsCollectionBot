dcfaea8b71b0d00cd8c631aec2fc58f262e6e1bd
17316267816 <origerous@live.com>
2025-06-20 14:19:42 +0800

    123

--- sync.py
+++ sync.py
@@ -4,6 +4,6 @@ import asyncio
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
 
-    synchronizer = StockKlineSynchronizer('10.26.0.8', '2000', 'hmcz', 'Hmcz_12345678')
+    synchronizer = StockKlineSynchronizer('10.26.0.8', '2000', 'hmcz', 'Hmcz_12345678', '../stock_sync_queue')
     loop.run_until_complete(synchronizer.connect_async())
     loop.run_until_complete(synchronizer.all_sync())
