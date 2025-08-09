4266ac55e9ff699f1f1469f130e4f439d8695854
origerous <origerous@live.com>
2025-08-09 09:33:41 +0800

    123

--- ai/vision/price_trend/models/stockcnn.py
+++ ai/vision/price_trend/models/stockcnn.py
@@ -107,9 +107,9 @@ class StockChartNet(nn.Module):
         )
 
         block2 = ResidualBlock(16, 32, stride=2, attention=False)
-        block3 = ResidualBlock(32, 64, stride=2)
+        block3 = ResidualBlock(32, 64, stride=2, attention=False)
         block4 = ResidualBlock(64, 128, stride=2)
-        block5 = ResidualBlock(128, 256, stride=2)
+        block5 = ResidualBlock(128, 256, stride=1)
 
         self.layers = nn.Sequential(
             stem,
