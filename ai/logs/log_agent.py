0d3f3403c5404fae44f63b7f87ab5e54663662f7
origerous <origerous@live.com>
2025-08-21 15:42:39 +0800

    123

--- ai/logs/log_agent.py
+++ ai/logs/log_agent.py
@@ -4,7 +4,7 @@ class LogAgent:
     def __init__(self, project_name, config, name='vision_price_trend'):
         swanlab.login(api_key='U8RYQtw9BGcj2r4yt6cD1')
 
-        swanlab.init(project=project_name, config=config, experiment_name=name, reinit=True, id=f'{project_name}_{name}', resume='allow')
+        swanlab.init(project=project_name, config=config, experiment_name=name, reinit=True, id=f'{project_name}{name}', resume='allow')
 
     def log(self, info):
         swanlab.log(info)
