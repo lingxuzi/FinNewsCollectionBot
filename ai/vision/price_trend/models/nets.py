cb0d8c28ea88ddaffa98d35fd38e84031d3fca36
origerous <origerous@live.com>
2025-08-22 20:42:42 +0800

    123

--- ai/vision/price_trend/models/nets.py
+++ ai/vision/price_trend/models/nets.py
@@ -210,7 +210,7 @@ class StockNet(nn.Module):
                 ts_logits = None
 
             ts_fused = self.fusion(x, ts_fused)
-            trend_logits_fused = self.trend_classifier_fused(ts_fused)
+            trend_logits_fused = (ts_logits + trend_logits) / 2 #self.trend_classifier_fused(ts_fused)
             if not self.infer_mode:
                 stock_logits = self.stock_classifier(ts_fused)
                 industry_logits = self.industry_classifier(ts_fused)
