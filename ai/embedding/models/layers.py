6131fdfa39b30e9e983ed8ad000ec6b7e85448dd
origerous <origerous@live.com>
2025-09-05 12:10:58 +0800

    123

--- ai/embedding/models/layers.py
+++ ai/embedding/models/layers.py
@@ -191,13 +191,11 @@ class LayerNormedResidualMLP(nn.Module):
             return self.act(self.p(x))
 
 class ALSTMEncoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_layers, embedding_dim, gru=False, kl=False, dropout=0.1, embedding=True):
+    def __init__(self, input_dim, hidden_dim, num_layers, embedding_dim, gru=False, kl=False, dropout=0.1):
         super().__init__()
         self.gru = gru
         self.kl = kl
         self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True) if not gru else nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True, dropout=dropout)
-        if kl:
-            self.embedding_logvar = nn.Linear(hidden_dim * 2, embedding_dim, bias=True)
         self.att_net = nn.Sequential(
             nn.Linear(in_features=hidden_dim, out_features=int(hidden_dim / 2)),
             nn.Dropout(dropout) if dropout > 0 else nn.Identity(),
@@ -205,9 +203,11 @@ class ALSTMEncoder(nn.Module):
             nn.Linear(in_features=int(hidden_dim / 2), out_features=1),
             nn.Softmax(dim=1)
         )
-        self.embedding = embedding
-        if self.embedding:
+        if self.kl:
             self.embedding_fc = nn.Linear(hidden_dim * 2, embedding_dim, bias=True)
+            self.embedding_logvar = nn.Linear(hidden_dim * 2, embedding_dim, bias=True)
+        else:
+            self.residual_fc = LayerNormedResidualMLP(hidden_dim * 2, embedding_dim)
 
     def forward(self, x):
         self.lstm.flatten_parameters()
@@ -218,17 +218,16 @@ class ALSTMEncoder(nn.Module):
         attention_score = self.att_net(out)
         out_att = out * attention_score
         out_att = torch.sum(out_att, dim=1)
-        if self.embedding:
+        if self.kl:
             embedding = self.embedding_fc(torch.cat([h[-1], out_att], dim=1))
-            if self.kl:
-                logvar = self.embedding_logvar(torch.cat([h[-1], out_att], dim=1))
-                if self.training:
-                    std = torch.exp(0.5 * logvar)
-                    eps = torch.randn_like(std)
-                    return eps.mul(std).add_(embedding), embedding, logvar
+            logvar = self.embedding_logvar(torch.cat([h[-1], out_att], dim=1))
+            if self.training:
+                std = torch.exp(0.5 * logvar)
+                eps = torch.randn_like(std)
+                return eps.mul(std).add_(embedding), embedding, logvar
             return embedding, None, None
         else:
-            return torch.cat([h[-1], out_att], dim=1), None, None
+            return self.residual_fc(torch.cat([h[-1], out_att], dim=1)), None, None
 
 class ALSTMDecoder(nn.Module):
     def __init__(self, output_dim, hidden_dim, num_layers, embedding_dim, gru=False):
--- ai/vision/price_trend/models/ts_encoder.py
+++ ai/vision/price_trend/models/ts_encoder.py
@@ -4,18 +4,20 @@ class TSEncoder(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
-        self.ts_model = ALSTMEncoder(config['ts_input_dim'], config['hidden_dim'], num_layers=config['num_layers'], embedding_dim=config['ts_embedding_dim'], gru=True, kl=False, dropout=0.1, embedding=True)
+        self.ts_model = ALSTMEncoder(config['ts_input_dim'], config['hidden_dim'], num_layers=config['num_layers'], embedding_dim=config['ts_embedding_dim'], gru=True, kl=False, dropout=0.1)
         self.ctx_model = ResidualMLPBlock(config['ctx_input_dim'], config['hidden_dim'], config['ctx_embedding_dim'],dropout_rate=0, use_batchnorm=True)
-        self.embedding_projector = nn.Linear(config['ts_embedding_dim'] + config['ctx_embedding_dim'], config['hidden_dim'])
-        self.embedding_norm = nn.LayerNorm(config['hidden_dim'])
-        self.fusion_block = ResidualMLPBlock(config['hidden_dim'], config['hidden_dim'] // 2, config['embedding_dim'], use_batchnorm=False, dropout_rate=config['dropout'])
+        # self.embedding_projector = nn.Linear(config['ts_embedding_dim'] + config['ctx_embedding_dim'], config['hidden_dim'])
+        # self.embedding_norm = nn.LayerNorm(config['hidden_dim'])
+
+        full_dim = config['ts_embedding_dim'] + config['ctx_embedding_dim']
+        self.fusion_block = ResidualMLPBlock(full_dim, full_dim // 2, config['embedding_dim'], use_batchnorm=True, dropout_rate=config['dropout'])
     
     def forward(self, x):
         ts_seq, ctx_seq = x
         ts_emb, _, _ = self.ts_model(ts_seq)
         ctx_emb = self.ctx_model(ctx_seq)
         emb = torch.cat([ts_emb, ctx_emb], dim=1)
-        emb = self.embedding_projector(emb)
-        emb = self.embedding_norm(emb)
+        # emb = self.embedding_projector(emb)
+        # emb = self.embedding_norm(emb)
         emb = self.fusion_block(emb)
         return emb
