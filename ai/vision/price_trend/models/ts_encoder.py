ae632fd59ffe8164e1b26d3ec2bcc4a964dd6bfa
origerous <origerous@live.com>
2025-09-05 12:04:35 +0800

    123

--- ai/vision/price_trend/models/ts_encoder.py
+++ ai/vision/price_trend/models/ts_encoder.py
@@ -6,14 +6,16 @@ class TSEncoder(nn.Module):
         self.config = config
         self.ts_model = ALSTMEncoder(config['ts_input_dim'], config['hidden_dim'], num_layers=config['num_layers'], embedding_dim=config['ts_embedding_dim'], gru=True, kl=False, dropout=0.1, embedding=False)
         self.ctx_model = ResidualMLPBlock(config['ctx_input_dim'], config['hidden_dim'], config['ctx_embedding_dim'],dropout_rate=0, use_batchnorm=True)
-        # self.embedding_projector = nn.Linear(config['ts_embedding_dim'] + config['ctx_embedding_dim'], config['hidden_dim'])
-        # self.embedding_norm = nn.LayerNorm(config['hidden_dim'])
-        self.fusion_block = LayerNormedResidualMLP(config['hidden_dim'] * 2 + config['ctx_embedding_dim'], config['embedding_dim'], dropout_rate=config['dropout'])
+        self.embedding_projector = nn.Linear(config['ts_embedding_dim'] + config['ctx_embedding_dim'], config['hidden_dim'])
+        self.embedding_norm = nn.LayerNorm(config['hidden_dim'])
+        self.fusion_block = ResidualMLPBlock(config['hidden_dim'], config['hidden_dim'] // 2, config['embedding_dim'], use_batchnorm=False, dropout_rate=config['dropout'])
     
     def forward(self, x):
         ts_seq, ctx_seq = x
         ts_emb, _, _ = self.ts_model(ts_seq)
         ctx_emb = self.ctx_model(ctx_seq)
         emb = torch.cat([ts_emb, ctx_emb], dim=1)
+        emb = self.embedding_projector(emb)
+        emb = self.embedding_norm(emb)
         emb = self.fusion_block(emb)
         return emb
